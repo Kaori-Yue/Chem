3c20a37db472fbf385904b8063585593d8ff2d25
CrossKnight <forgetmenotforevertime@hotmail.com>
2018-10-30 18:50:50 +0700

    move queue to usercontrol

---
+++ Chem/UserControl/Queue.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+
+namespace Chem.UserControl {
+
+	/// <summary>
+	/// Interaction logic for Queue.xaml
+	/// </summary>
+	public partial class Queue {
+		//public WindowCommandsOverlayBehavior LeftWindowCommandsOverlayBehavior;
+		//public WindowCommandsOverlayBehavior RightWindowCommandsOverlayBehavior;
+		//public WindowCommandsOverlayBehavior WindowButtonCommandsOverlayBehavior;
+		//public WindowCommandsOverlayBehavior IconOverlayBehavior;
+		public Queue() {
+			//WindowCommandsOverlayBehavior.Always;
+			InitializeComponent();
+		}
+	}
+}
